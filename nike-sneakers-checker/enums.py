c7b98cb887205cd94e4b5bce1968dc20defea3ea
ivanmikhailov <mikhailovj@gmail.com>
2021-12-16 00:13:07 +0300

    add enums

---
+++ nike-sneakers-checker/enums.py
@@ -0,0 +1,9 @@
+import enum
+
+
+class LoginAttrs(enum.Enum):
+    ENTER_BUTTON_ID = 'hf_title_signin_membership'
+    NIKE_LOGIN_ID = '80ad225a-df94-4448-8e93-4681b0191b5f'
+    NIKE_PASSWORD_ID = 'a9814734-3307-4253-8bf0-fd12951bb7f3'
+    SUBMIT_BUTTON_ID = 'e589f9fd-c32a-4135-abcc-2a51b2138398'
+    KIP_ME_LOGGED_ID_CHECKBOX_NAME = 'keepMeLoggedIn'
--- nike-sneakers-checker/login.py
+++ nike-sneakers-checker/login.py
@@ -1,7 +1,8 @@
+from app.config import config
+from enums import LoginAttrs
+
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
-
-from app.config import config
 
 
 def login(browser):
@@ -9,22 +10,24 @@ def login(browser):
     browser.get(config.NIKE_SITE_URL)
 
     wait = WebDriverWait(browser, timeout=config.MAX_WAIT_ELEMENT_APPEARANCE_SEC)
-    wait.until(lambda p: p.find_element(By.ID, value='hf_title_signin_membership'))
+    wait.until(lambda p: p.find_element(By.ID, value=LoginAttrs.ENTER_BUTTON_ID.value))
 
-    enter_button = browser.find_element(By.ID, value='hf_title_signin_membership')
+    enter_button = browser.find_element(By.ID, value=LoginAttrs.ENTER_BUTTON_ID.value)
     enter_button.click()
 
-    nike_login = browser.find_element(By.ID, value='80ad225a-df94-4448-8e93-4681b0191b5f')
-    nike_password = browser.find_element(By.ID, value='a9814734-3307-4253-8bf0-fd12951bb7f3')
+    wait.until(lambda p: p.find_element(By.ID, value=LoginAttrs.NIKE_LOGIN_ID))
+
+    nike_login = browser.find_element(By.ID, value=LoginAttrs.NIKE_LOGIN_ID)
+    nike_password = browser.find_element(By.ID, value=LoginAttrs.NIKE_PASSWORD_ID)
 
     nike_login.send_keys(config.NIKE_SITE_LOGIN)
     nike_password.send_keys(config.NIKE_SITE_PASSWORD)
 
-    kip_me_logged_in = browser.find_element(By.NAME, value='keepMeLoggedIn')
+    kip_me_logged_in = browser.find_element(By.NAME, value=LoginAttrs.KIP_ME_LOGGED_ID_CHECKBOX_NAME)
 
     is_selected = kip_me_logged_in.get_property('checked')
     if is_selected:
         kip_me_logged_in.click()
 
-    submit_button = browser.find_element(By.ID, value='e589f9fd-c32a-4135-abcc-2a51b2138398')
+    submit_button = browser.find_element(By.ID, value=LoginAttrs.SUBMIT_BUTTON_ID)
     submit_button.click()
